e9ff2e7aa0fbc778dd972076cfa0e9ed440fa23a
MikeY700 <spatamike@hotmail.com>
2023-06-19 17:55:09 +0700

    [ADD] : healthcheck db

--- ShoppingCartAPI/ShoppingCartAPI/Program.cs
+++ ShoppingCartAPI/ShoppingCartAPI/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using ShoppingCartAPI.DataAccess;
 using ShoppingCartAPI.Services;
 using ShoppingCartAPI.Services.Interfaces;
+using System.Text.Json;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +28,9 @@ builder.Configuration.GetConnectionString("DemoContext")));
 builder.Services.AddScoped<ITransactionService, TransactionService>();
 builder.Services.AddScoped<IProductService, ProductService>();
 
+builder.Services.AddHealthChecks()
+    .AddDbContextCheck<ProductContext>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,4 +44,47 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.MapHealthChecks("/healthz", new HealthCheckOptions
+{
+    ResponseWriter = async (context, report) =>
+    {
+        context.Response.ContentType = "application/json; charset=utf-8";
+
+        var options = new JsonWriterOptions { Indented = true };
+
+        using var memoryStream = new MemoryStream();
+        using (var jsonWriter = new Utf8JsonWriter(memoryStream, options))
+        {
+            jsonWriter.WriteStartObject();
+            jsonWriter.WriteString("status", report.Status.ToString());
+            jsonWriter.WriteStartObject("results");
+
+            foreach (var healthReportEntry in report.Entries)
+            {
+                jsonWriter.WriteStartObject(healthReportEntry.Key);
+                jsonWriter.WriteString("status",
+                    healthReportEntry.Value.Status.ToString());
+                jsonWriter.WriteString("description",
+                    healthReportEntry.Value.Description);
+                jsonWriter.WriteStartObject("data");
+
+                foreach (var item in healthReportEntry.Value.Data)
+                {
+                    jsonWriter.WritePropertyName(item.Key);
+
+                    JsonSerializer.Serialize(jsonWriter, item.Value,
+                        item.Value?.GetType() ?? typeof(object));
+                }
+
+                jsonWriter.WriteEndObject();
+                jsonWriter.WriteEndObject();
+            }
+
+            jsonWriter.WriteEndObject();
+            jsonWriter.WriteEndObject();
+        }
+
+        await context.Response.WriteAsync(Encoding.UTF8.GetString(memoryStream.ToArray()));
+    }
+});
 app.Run();
